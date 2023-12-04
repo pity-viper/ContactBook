84627f0fc9b96c49057b7aaa658389d2f00b4b63
schoolyo <112569021+schoolyo@users.noreply.github.com>
2023-12-04 10:56:10 -0600

    created main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+class contact:
+    def __init__(self, firstN, lastN, phoneN, addres = ""):
+        self.firstName = firstN
+        self.lastName = lastN
+        self.phoneNumber = phoneN
+        self.address = addres
 
+    def toString(self):
+        if self.address == "":
+            print(self.firstName + self.lastName + ", " + self.phoneNumber)
+        else:
+            print(self.firstName + self.lastName + ", " + self.phoneNumber + ", Address: " + self.address)
+
+class contactBook:
+    def __init__

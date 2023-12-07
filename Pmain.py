55840c723ff9a9170b21039f48b67852a16dbb5a
056475 <youngp6475@g.comalisd.org>
2023-12-07 14:47:23 -0600

    updating helper methods and created getContacts method

--- Pmain.py
+++ Pmain.py
@@ -20,7 +20,8 @@ class ContactNode:
         #self.children = {k:None for k in string.ascii_lowercase}
         self.children = {}
         self.endWord = False
-        self.endNode = False
+        self.leafNode = False
+        self.endNumber = False
         self.contacts = []
 
 
@@ -30,24 +31,37 @@ class ContactBook:
         self.root = ContactNode()
 
     def insert(self, contact: Contact) -> None:
-        current = self.root
-        self._insertName(current, contact.firstName)
-        self._insertName(current, contact.lastName)
-        self._insertPhoneNumber(current, contact.phoneNumber)
+        self.__insertName(contact.firstName)
+        self.__insertName(contact.lastName)
+        self.__insertPhoneNumber(contact.phoneNumber)
 
-    def _insertName(self, current: ContactNode, name: string) -> None:
+    def __insertName(self, name: string) -> None:
+        current = self.root
         for letter in name:
             if not current.children.get(letter):
                 current.children[letter] = ContactNode()
             current = current.children[letter]
         current.endWord = True
-        current.endNode = True
+        current.leafNode = True # add logic to flip this off if another node is added after it
         # need to add the contact to contacts list in node
 
-    def _insertPhoneNumber(self, current: ContactNode, phoneNumber: string) -> None:
-        pass
+    def __insertPhoneNumber(self, phoneNumber: string) -> None:
+        current = self.root
+        for number in phoneNumber:
+            if not current.children.get(number):
+                current.children[number] = ContactNode()
+            current = current.children[number]
+        current.endNumber = True
+        current.leafNode = True
+        # need to add the contact to the contacts list in node
+        # need to make sure that this doesn't conflict with the name trie. Either have 2 tries or all in one somehow
+
     def search(self, contact: Contact) -> list:
         pass
 
     def delete(self, contact: Contact) -> None:
         pass
+
+    def getContacts(self) -> list:
+        pass
+        # like a toString method, but instead interates through the trie to get all the contacts
